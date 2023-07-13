a7d997573b165658415a7b4d65bedef4e46997e1
Alejandro Palmar <thepalmars1one@gmail.com>
2023-07-13 02:01:22 -0400

    Powerstats and Genders Filters

--- src/app/render-heroes/render-heroes.component.ts
+++ src/app/render-heroes/render-heroes.component.ts
@@ -9,8 +9,12 @@ import { HeroesService } from '../services/heroes.service';
 })
 export class RenderHeroesComponent{
 
-  allHeroes: any= []
-
+  allHeroes: any= [];
+  actualHeroes: any[] = [];
+  categories: string[] = [];
+  genders: any[] = [];
+  races: any[]=[];
+  showPowerstatButtons = false;
 
   constructor(private _heroesService: HeroesService) {}
 
@@ -19,6 +23,9 @@ export class RenderHeroesComponent{
       const heroes = await this._heroesService.getHeroes();
       console.log(heroes);
       this.allHeroes.push(...heroes); // Agrega los héroes individualmente
+      this.actualHeroes = this.allHeroes;
+      this.getCategoriesFromHeroes();
+      this.getGendersFromHeroes();
     } catch (error) {
       // Manejar cualquier error que pueda ocurrir durante la llamada a la API.
     }
@@ -27,6 +34,57 @@ export class RenderHeroesComponent{
   ngOnInit() {
     this.loadHeroes();
   }
+
+  getHeroesByPowerstats(category: string) {
+    
+    const filteredHeroes = this.allHeroes.filter((hero: any) => {
+      return hero.powerstats.hasOwnProperty(category);
+    });
+    
+    filteredHeroes.sort((a: any, b: any) => {
+      const powerstatA = parseInt(a.powerstats[category]);
+      const powerstatB = parseInt(b.powerstats[category]);
+      
+      return powerstatB - powerstatA;
+    });
+    
+    this.actualHeroes = filteredHeroes;
+  }
+
+  getCategoriesFromHeroes() {
+
+    const powerstatsKeys = Object.keys(this.allHeroes[0].powerstats);
+    this.categories = powerstatsKeys;
+    console.log(powerstatsKeys);
+}
+
+  getHeroesByGenders(gender: string){
+
+  const filteredGenders = this.allHeroes.filter((hero: any) => {
+    return hero.appearance.gender.toLowerCase() === gender.toLowerCase();
+  });
+
+  this.actualHeroes = filteredGenders;
+
+}
+
+  getGendersFromHeroes() {
+  const genders = this.allHeroes.map((hero: any) => {
+    return hero.appearance.gender.toLowerCase();
+  });
+
+  // Utilizar Set para obtener los géneros únicos
+  const singleGenders = Array.from(new Set(genders));
+
+  this.genders = singleGenders;
+  console.log(this.genders);
+}
+
+getRacesFromHeroes() {
   
+}
+
+
+
 
 }
